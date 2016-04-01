7923a546924218db56929157d67bfd87ff4e3747
Bernardo Martínez Garrido <programming@wandrell.com>
2016-04-01 13:47:33 +0200

    Added an entity using a converter.

---
+++ src/main/java/com/wandrell/example/jpa/model/converter/BooleanTFConverter.java
@@ -0,0 +1,23 @@
+
+package com.wandrell.example.jpa.model.converter;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class BooleanTFConverter implements AttributeConverter<Boolean, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Boolean value) {
+        if (Boolean.TRUE.equals(value)) {
+            return "T";
+        } else {
+            return "F";
+        }
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String value) {
+        return "T".equals(value);
+    }
+}
