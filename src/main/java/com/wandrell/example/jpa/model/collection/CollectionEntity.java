4683efff0a1426e652552ef5788619785590f3fb
Bernardo Martínez Garrido <programming@wandrell.com>
2016-04-09 16:14:55 +0200

    Divided the multi value entity into one for maps and another for collections.

--- src/main/java/com/wandrell/example/jpa/model/collection/CollectionEntity.java
+++ src/main/java/com/wandrell/example/jpa/model/collection/CollectionEntity.java
@@ -25,9 +25,7 @@
 package com.wandrell.example.jpa.model.collection;
 
 import java.util.Collection;
-import java.util.LinkedHashMap;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -41,7 +39,7 @@ import com.google.common.base.MoreObjects;
 
 @Entity(name = "MultiValueEntity")
 @Table(name = "multivalue_entities")
-public class MultiValueEntity {
+public class CollectionEntity {
 
     /**
      * Entity's ID.
@@ -54,10 +52,7 @@ public class MultiValueEntity {
     @Column(name = "integersCol", nullable = false)
     private Collection<Integer>  integersCol = new LinkedList<Integer>();
 
-    @Column(name = "integersMap", nullable = false)
-    private Map<String, Integer> integersMap = new LinkedHashMap<String, Integer>();
-
-    public MultiValueEntity() {
+    public CollectionEntity() {
         super();
     }
 
@@ -75,16 +70,12 @@ public class MultiValueEntity {
             return false;
         }
 
-        final MultiValueEntity other = (MultiValueEntity) obj;
+        final CollectionEntity other = (CollectionEntity) obj;
         return Objects.equals(id, other.id);
     }
 
     public final Collection<Integer> getIntegersCol() {
         return integersCol;
-    }
-
-    public final Map<String, Integer> getIntegersMap() {
-        return integersMap;
     }
 
     @Override
@@ -94,10 +85,6 @@ public class MultiValueEntity {
 
     public final void setIntegersCol(final Collection<Integer> integersCol) {
         this.integersCol = integersCol;
-    }
-
-    public final void setIntegersMap(final Map<String, Integer> integersMap) {
-        this.integersMap = integersMap;
     }
 
     @Override
---
+++ src/main/java/com/wandrell/example/jpa/model/collection/MapEntity.java
@@ -0,0 +1,95 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2016 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.wandrell.example.jpa.model.collection;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.google.common.base.MoreObjects;
+
+@Entity(name = "MultiValueEntity")
+@Table(name = "multivalue_entities")
+public class MapEntity {
+
+    /**
+     * Entity's ID.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private Integer              id          = null;
+
+    @Column(name = "integersMap", nullable = false)
+    private Map<String, Integer> integersMap = new LinkedHashMap<String, Integer>();
+
+    public MapEntity() {
+        super();
+    }
+
+    @Override
+    public final boolean equals(final Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        final MapEntity other = (MapEntity) obj;
+        return Objects.equals(id, other.id);
+    }
+
+    public final Map<String, Integer> getIntegersMap() {
+        return integersMap;
+    }
+
+    @Override
+    public final int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public final void setIntegersMap(final Map<String, Integer> integersMap) {
+        this.integersMap = integersMap;
+    }
+
+    @Override
+    public final String toString() {
+        return MoreObjects.toStringHelper(this).add("id", id).toString();
+    }
+
+}
