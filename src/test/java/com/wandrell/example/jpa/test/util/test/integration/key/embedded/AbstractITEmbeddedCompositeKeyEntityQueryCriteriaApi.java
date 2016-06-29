fb1e08f345fe69fe99aa13ab50e0933c90a11f4f
Bernardo Martínez Garrido <programming@wandrell.com>
2016-06-29 11:27:09 +0200

    Added base tests for the remaining key entities.

---
+++ src/test/java/com/wandrell/example/jpa/test/util/test/integration/key/embedded/AbstractITEmbeddedCompositeKeyEntityQueryCriteriaApi.java
@@ -0,0 +1,100 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2015 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.wandrell.example.jpa.test.util.test.integration.key.embedded;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.testng.AbstractTransactionalTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.wandrell.example.jpa.model.key.embedded.EmbeddedCompositeKeyEntity;
+import com.wandrell.example.jpa.test.util.criteria.key.embedded.EmbeddedCompositeKeyEntityCriteriaFactory;
+
+/**
+ * Abstract integration tests for a {@link EmbeddedCompositeKeyEntity} testing
+ * it loads values correctly by using the criteria API.
+ * <p>
+ * Checks the following cases:
+ * <ol>
+ * <li>Retrieving an entity by its ids returns the correct one.</li>
+ * </ol>
+ * <p>
+ * This is meant to be used along a Spring context, which will set up the
+ * repository and all of it's requirements.
+ *
+ * @author Bernardo Martínez Garrido
+ * @see EmbeddedCompositeKeyEntity
+ */
+public abstract class AbstractITEmbeddedCompositeKeyEntityQueryCriteriaApi
+        extends AbstractTransactionalTestNGSpringContextTests {
+
+    /**
+     * The JPA entity manager.
+     */
+    @Autowired
+    private EntityManager entityManager;
+
+    /**
+     * Default constructor.
+     */
+    public AbstractITEmbeddedCompositeKeyEntityQueryCriteriaApi() {
+        super();
+        // TODO: Add the query results counts to the configuration files
+    }
+
+    /**
+     * Tests that retrieving an entity by its ids returns the correct one.
+     */
+    @Test
+    public final void testFindByIds() {
+        final Query query;                      // Query for the entity
+        final EmbeddedCompositeKeyEntity entity; // The entity
+
+        // Builds the query
+        query = getEntityManager()
+                .createQuery(EmbeddedCompositeKeyEntityCriteriaFactory
+                        .findByIds(entityManager, 1, 2));
+
+        // Acquires the entity
+        entity = (EmbeddedCompositeKeyEntity) query.getSingleResult();
+
+        // The number of results is the expected one
+        Assert.assertEquals(entity.getKey().getId(), new Integer(1));
+        Assert.assertEquals(entity.getKey().getSupportId(), new Long(2));
+    }
+
+    /**
+     * Returns the JPA entity manager.
+     *
+     * @return the JPA entity manager
+     */
+    protected final EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+}
