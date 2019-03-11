83785f2e9e559deec46800ecb51f21008f02d060
Bernardo <programming@wandrell.com>
2019-03-11 22:15:42 +0100

    Changed license dates

--- src/test/java/com/bernardomg/example/jpa/test/integration/embedded/embedded/ITEmbeddedEntityQueryJpql.java
+++ src/test/java/com/bernardomg/example/jpa/test/integration/embedded/embedded/ITEmbeddedEntityQueryJpql.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  * <p>
- * Copyright (c) 2016-2017 the the original author or authors.
+ * Copyright (c) 2016-2019 the the original author or authors.
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
