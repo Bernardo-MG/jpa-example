7968f7ef7bb98216b85d8b35200dc564e443df44
Bernardo <programming@bernardomg.com>
2021-09-17 23:43:00 +0200

    Updated license dates

--- src/test/java/com/bernardomg/example/jpa/test/util/criteria/value/BooleanEntityCriteriaFactory.java
+++ src/test/java/com/bernardomg/example/jpa/test/util/criteria/value/BooleanEntityCriteriaFactory.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  * <p>
- * Copyright (c) 2016-2019 the the original author or authors.
+ * Copyright (c) 2016-2021 the the original author or authors.
  * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
